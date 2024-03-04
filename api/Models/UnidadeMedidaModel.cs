a78554eb03b6c96b1de6669606906c2a29367179
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2024-03-04 10:20:31 -0300

    feat(UnidadeMedidaModel): codes to create the model and establish the connection to the database

---
+++ api/Models/UnidadeMedidaModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models;
+
+[Table("unidademedida")]
+
+public class UnidadeMedidaModel
+{
+
+    [Key]
+    [Column("unidademedidaid")]
+    public int Id { get; set; }
+
+    [Column("descrição")]
+    public string Descricao { get; set; }
+
+    [Column("abreviatura")]
+    public string Abreviatura { get; set; }
+}
--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede113722d47f762bfa08ec05350b24b9d920bc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
