f43939bb7ae489d1c0069d014916f05e7536d141
marcogitti <marcoalcindogitti@gmail.com>
2024-03-04 14:56:34 -0300

    feat(UnidadeConsumidoraDTO):Creation of the UnidadeConsumidoraDTO and creation of codes for the relationship with the Model

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36229abb9793973f41d9f8a082d6a41c530eb67b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59605c26d76fa4b046dcfe436fd83520d810d1c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
