f3af99e549071fd8c45c7a1ba97fb3f3c8653b69
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2024-03-04 10:40:24 -0300

    feat(UnidadeMedidaDTO): codes to limit the model's business rule

--- api/Controllers/UnidadeMedidaController.cs
+++ api/Controllers/UnidadeMedidaController.cs
@@ -1,6 +1,60 @@
-﻿namespace api.Controllers
+﻿using api.DTO.Entities;
+using api.Models;
+using api.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
 {
-    public class UnidadeMedidaController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnidadeMedidaController : ControllerBase
     {
+        private readonly IUnidadeMedidaService _unidadeMedidaService;
+
+        public UnidadeMedidaController(IUnidadeMedidaService UnidadeMedidaService)
+        {
+            _unidadeMedidaService = UnidadeMedidaService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UnidadeMedidaDTO>>> BuscarTodosUnidadeMedida()
+        {
+            var unidadeMedidas = await _unidadeMedidaService.BuscarTodosUnidadeMedida();
+            return Ok(unidadeMedidas);
+        }
+
+        [HttpGet("{id}", Name = "GetUnidadeMedida")]
+        public async Task<ActionResult<UnidadeMedidaModel>> BuscarPorId(int id)
+        {
+            var unidadeMedida = await _unidadeMedidaService.BuscarPorId(id);
+            if (unidadeMedida == null) return NotFound("Unidade de Medida não encontrada");
+            return Ok(unidadeMedida);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Cadastrar([FromBody] UnidadeMedidaDTO unidadeMedidaDTO)
+        {
+            if (unidadeMedidaDTO is null) return BadRequest("dado inválido");
+            await _unidadeMedidaService.Adicionar(unidadeMedidaDTO);
+            return new CreatedAtRouteResult("GetUnidadeMedida", new { id = unidadeMedidaDTO.Id }, unidadeMedidaDTO);
+        }
+
+        [HttpPut()]
+        public async Task<ActionResult<UnidadeMedidaModel>> Atualizar([FromBody] UnidadeMedidaDTO unidadeMedidaDTO)
+        {
+            if (unidadeMedidaDTO is null) return BadRequest("dado inválido");
+            await _unidadeMedidaService.Atualizar(unidadeMedidaDTO);
+            return Ok(unidadeMedidaDTO);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UnidadeMedidaModel>> Apagar(int id)
+        {
+            var unidadeMedidaDTO = await _unidadeMedidaService.BuscarPorId(id);
+            if (unidadeMedidaDTO == null) return BadRequest("Unidade de Medida não encontrada");
+            await _unidadeMedidaService.Apagar(id);
+            return Ok(unidadeMedidaDTO);
+        }
+
     }
 }
