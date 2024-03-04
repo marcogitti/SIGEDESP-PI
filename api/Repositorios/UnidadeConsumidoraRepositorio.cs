186ac01add23b23e402e40389161abf9744a9e0d
marcogitti <marcoalcindogitti@gmail.com>
2024-03-04 15:11:18 -0300

    feat(Repositorios):Creation of UnidadeConsumidora repositories

---
+++ api/Repositorios/UnidadeConsumidoraRepositorio.cs
@@ -0,0 +1,55 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+using api.Data;
+using api.Repositorios.Interfaces;
+
+namespace api.Repositorios
+{
+    public class UnidadeConsumidoraRepositorio : IUnidadeConsumidoraRepositorio
+
+        private readonly SigedespDBContex _dbContext;
+        public UnidadeConsumidoraRepositorio(SigedespDBContex sigedespDBContex)
+        {
+            _dbContext = sigedespDBContex;
+        }
+
+        public async Task<UnidadeConsumidoraModel> BuscarPorId(int id)
+        {
+            return await _dbContext.UnidadeConsumidora.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<UnidadeConsumidoraModel>> BuscarTodosUnidadeConsumidora()
+        {
+            return await _dbContext.UnidadeConsumidora.ToListAsync();
+        }
+
+        public async Task<UnidadeConsumidoraModel> Adicionar(UnidadeConsumidoraModel unidadeConsumidora)
+        {
+            await _dbContext.UnidadeConsumidora.AddAsync(unidadeConsumidora);
+            await _dbContext.SaveChangesAsync();
+
+            return unidadeConsumidora;
+        }
+
+        public async Task<UnidadeConsumidoraModel> Atualizar(UnidadeConsumidoraModel unidadeConsumidora)
+        {
+            _dbContext.Entry(unidadeConsumidora).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return unidadeConsumidora;
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            UnidadeConsumidoraModel unidadeConsumidoraPorId = await BuscarPorId(id);
+
+            if (unidadeConsumidoraPorId == null)
+            {
+                throw new Exception($"Unidade Consumidora para o ID: {id} não foi encontrada no banco de dados");
+            }
+
+            _dbContext.UnidadeConsumidora.Remove(unidadeConsumidoraPorId);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
