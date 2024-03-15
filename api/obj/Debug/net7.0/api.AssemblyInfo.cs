6f56ef04f0ca9d67c82890b4f28ec66e367d191e
marcogitti <marcoalcindogitti@gmail.com>
2024-03-15 15:45:21 -0300

    feat(ISecretariaRepositorio): Creation of an interface for the Secretaria class to define asynchronous methods for CRUD operations

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+703f82be7d5f87b90654646f6f54ec5f0fba91b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28db78cab78a42ce5563a5ae1d9dce3150a141c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
