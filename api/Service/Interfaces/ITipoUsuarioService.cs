978f835b091bd25d25800c2b2432e0fd3342f3e7
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2024-03-15 10:18:31 -0300

    feat(ITipoUsuarioService): The Tipo Usuario Service Interface was created, and the necessary code was implemented for its functionalities

---
+++ api/Service/Interfaces/ITipoUsuarioService.cs
@@ -0,0 +1,13 @@
+﻿using api.DTO.Entities;
+
+namespace api.Service.Interfaces
+{
+    public interface ITipoUsuarioService
+    {
+        Task<IEnumerable<TipoUsuarioDTO>> BuscarTodosTipoUsuario();
+        Task<TipoUsuarioDTO> BuscarPorId(int id);
+        Task Adicionar(TipoUsuarioDTO TipoUsuarioDTO);
+        Task Atualizar(TipoUsuarioDTO TipoUsuarioDTO);
+        Task Apagar(int id);
+    }
+}
