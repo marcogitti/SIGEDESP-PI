703f82be7d5f87b90654646f6f54ec5f0fba91b3
marcogitti <marcoalcindogitti@gmail.com>
2024-03-15 15:13:15 -0300

    feat(SecretariaModel): Created the model for the Secretaria class and developed the code for its creation in the database

---
+++ api/Models/SecretariaModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models;
+
+[Table("secretaria")]
+
+public class SecretariaModel
+{
+
+    [Key]
+    [Column("secretariaid")]
+    public int Id { get; set; }
+
+    [Column("situacao")]
+    public string Situacao { get; set; }
+
+    [Column("descricao")]
+    public string Descricao { get; set; }
+}
