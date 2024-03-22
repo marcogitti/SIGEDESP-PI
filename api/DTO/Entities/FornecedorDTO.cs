439ef14559a4805df9d822d71a43a38c93cd5c71
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2024-03-22 09:44:16 -0300

    [200~feat(FornecedorDTO): The DTO for the Fornecedor class was created, and the code was

---
+++ api/DTO/Entities/FornecedorDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace api.DTO.Entities
+{
+    public class FornecedorDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome Fantasia do Fornecedor é requerida!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string NomeFantasia { get; set; }
+
+        [Required(ErrorMessage = "A Situação é requerida!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Situacao { get; set; }
+
+    }
+}
+
