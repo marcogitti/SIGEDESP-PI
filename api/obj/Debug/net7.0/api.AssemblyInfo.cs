d73f38bcf34aa32e2bcb1cc07acbb09ccd5efce4
marcogitti <marcoalcindogitti@gmail.com>
2024-03-26 11:18:26 -0300

    feat(InstituicaoModel): Creation of InstituicaoModel and implementation of codes for defining variables

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7481f2272be776c5c70f89e743f55a1761210f03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd6061e45e9c4d6173291ca29e800c7ed91ede0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
