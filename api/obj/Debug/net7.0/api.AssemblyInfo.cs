014e02ea0e7007f99d6547652fcf7ff726ad5586
marcogitti <marcoalcindogitti@gmail.com>
2024-03-26 11:39:04 -0300

    feat(InstituicaoService): Creation of the Service for the Institution class and implementation of features in the code

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b3b76d0efd97978854b6a1c1982137250eb470")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d1b789a1e02aecece400eb51e371c6f73d2323")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
