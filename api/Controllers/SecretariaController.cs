a887f6a12fabe6c7d4405333033349b0dad60e79
marcogitti <marcoalcindogitti@gmail.com>
2024-04-09 19:27:36 -0300

    feat(SecretariaController): Updating the URI from Secretaria to secretaria in the secretaria controller

--- api/Controllers/SecretariaController.cs
+++ api/Controllers/SecretariaController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/secretaria")]
     [ApiController]
     public class SecretariaController : ControllerBase
     {
