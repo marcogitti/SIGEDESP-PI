6e8b2b895824631142beb251e22ccdba82d27b01
marcogitti <marcoalcindogitti@gmail.com>
2024-04-09 19:30:48 -0300

    feat(TipoDespesaController): Updating the URI from TipoDespesa to tipodespesa in the TipoDespesa controller

--- api/Controllers/TipoDespesaController.cs
+++ api/Controllers/TipoDespesaController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/tipodespesa")]
     [ApiController]
     public class TipoDespesaController : ControllerBase
     {
