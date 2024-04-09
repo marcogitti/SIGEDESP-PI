48f7950acc6d3e34c42a7889097bc619ad52c1fa
marcogitti <marcoalcindogitti@gmail.com>
2024-04-09 19:41:27 -0300

    feat(UnidadeMedidaController): Updating the URI from UnidadeMedida to unidademedida in the UnidadeMedida controller

--- api/Controllers/UnidadeMedidaController.cs
+++ api/Controllers/UnidadeMedidaController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/unidademedida")]
     [ApiController]
     public class UnidadeMedidaController : ControllerBase
     {
