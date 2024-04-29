f6fc3a96628e6a77aadd20a9b1b73ed091c2c0ed
gabyferr <70179462+gabyferr@users.noreply.github.com>
2024-04-29 15:10:56 -0300

    refactor(IService): IService Refactoring

--- front/lib/app/service/service.dart
+++ front/lib/app/service/service.dart
@@ -3,52 +3,103 @@ import 'dart:convert';
 import 'package:dson_adapter/dson_adapter.dart';
 import 'package:result_dart/result_dart.dart';
 
-class IService<T> {
+class IService<T extends Object> {
   final String path;
-  final Function classModel;
 
-  IService({required this.path, required this.classModel});
+  IService({required this.path});
 
-  AsyncResult<List<T>, String> getById(int id) async {
-    final response =
-        await http.get(Uri.http('https://localhost:7274/api/$path'));
+  Future<Result<List<T>, String>> getById(int id, classModel) async {
+    try {
+      final response =
+          await http.get(Uri.http('https://localhost:7274/api/$path'));
 
-    if (response.statusCode == 200) {
-      return (DSON().fromJson(response.body, classModel)).toSuccess();
-    } else {
-      return 'Falha ao carregar dados da API'.toFailure();
+      if (response.statusCode == 200) {
+        final decodedData = jsonDecode(response.body);
+        final List<T> dataList = List<T>.from(
+            decodedData.map((item) => const DSON().fromJson(item, classModel)));
+        return Result<List<T>, String>.success(dataList);
+      } else {
+        return Result<List<T>, String>.failure(
+            'Falha ao carregar dados da API');
+      }
+    } catch (e) {
+      return Result<List<T>, String>.failure(
+          'Erro ao processar requisição: $e');
     }
   }
 
-  getAll() {}
+  Future<Result<List<T>, String>> getAll(classModel) async {
+    try {
+      final response =
+          await http.get(Uri.http('https://localhost:7274/api/$path'));
 
-  // Future Result<List<T>, String> getAll() async {
-  //   final response =
-  //       await http.get(Uri.http('https://localhost:7274/api/$path'));
-
-    // if (true) {
-    //   final List<Map> resp = [
-    //     {"id": 0, "tipoDespesa": "string", "solicitaUC": "string"},
-    //     {"id": 1, "tipoDespesa": "carro", "solicitaUC": "string"},
-    //   ];
-    //   return resp
-    //       .map((e) => DSON().fromJson(e, classModel))
-    //       .toList()
-    //       .cast<T>()
-    //       .toSuccess();
-    // } else {
-    //   return 'Falha ao carregar dados da API'.toFailure();
-    // }
+      if (response.statusCode == 200) {
+        final decodedData = jsonDecode(response.body);
+        final List<T> dataList = List<T>.from(
+            decodedData.map((item) => const DSON().fromJson(item, classModel)));
+        return Result<List<T>, String>.success(dataList);
+      } else {
+        return Result<List<T>, String>.failure(
+            'Falha ao carregar dados da API');
+      }
+    } catch (e) {
+      return Result<List<T>, String>.failure(
+          'Erro ao processar requisição: $e');
+    }
   }
 
-  Future<T?> postDados(Map body) async {
-    final response = await http
-        .post(Uri.http('https://localhost:7274/api/$path'), body: body);
+  Future<Result<T, String>> postData(T data) async {
+    try {
+      final response = await http.post(
+        Uri.http('https://localhost:7274/api/$path'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(data),
+      );
 
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception('Falha ao carregar dados da API');
+      if (response.statusCode == 201) {
+        return Result<T, String>.success(data);
+      } else {
+        return Result<T, String>.failure('Falha ao enviar dados para a API');
+      }
+    } catch (e) {
+      return Result<T, String>.failure('Erro ao processar requisição: $e');
+    }
+  }
+
+  Future<Result<T, String>> editData(int id, T data) async {
+    try {
+      final response = await http.put(
+        Uri.http('https://localhost:7274/api/$path/$id'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode == 200) {
+        return Result<T, String>.success(data);
+      } else {
+        return Result<T, String>.failure('Falha ao editar dados na API');
+      }
+    } catch (e) {
+      return Result<T, String>.failure('Erro ao processar requisição: $e');
+    }
+  }
+
+  Future<Result<String, String>> deleteData(int id) async {
+    try {
+      final response =
+          await http.delete(Uri.http('https://localhost:7274/api/$path/$id'));
+
+      if (response.statusCode == 200) {
+        return 'apagado com sucesso'.toSuccess();
+      } else {
+        return 'Falha ao excluir dados na API'.toFailure();
+      }
+    } catch (e) {
+      return 'Erro ao processar requisição: $e'.toFailure();
     }
   }
 }
