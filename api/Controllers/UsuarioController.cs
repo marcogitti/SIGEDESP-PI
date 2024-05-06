1e206bfa2a6159666c9a9ccf03a549c403355e8e
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2024-05-06 10:20:02 -0300

    feat(UsuarioController): The Usuario Controller was created, and the codes were implemented to define a controller to handle operations related to Usuario

---
+++ api/Controllers/UsuarioController.cs
@@ -0,0 +1,60 @@
+﻿using api.DTO.Entities;
+using api.Models;
+using api.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioDTO>>> BuscarTodosUsuario()
+        {
+            var usuario = await _usuarioService.BuscarTodosUsuario();
+            return Ok(usuario);
+        }
+
+        [HttpGet("{id}", Name = "GetUsuario")]
+        public async Task<ActionResult<UsuarioModel>> BuscarPorId(int id)
+        {
+            var usuario = await _usuarioService.BuscarPorId(id);
+            if (usuario == null) return NotFound("Usuário não encontrado");
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Cadastrar([FromBody] UsuarioDTO usuarioDTO)
+        {
+            if (usuarioDTO is null) return BadRequest("dado inválido");
+            await _usuarioService.Adicionar(usuarioDTO);
+            return new CreatedAtRouteResult("GetUsuario", new { id = usuarioDTO.Id }, usuarioDTO);
+        }
+
+        [HttpPut()]
+        public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioDTO usuarioDTO)
+        {
+            if (usuarioDTO is null) return BadRequest("dado inválido");
+            await _usuarioService.Atualizar(usuarioDTO);
+            return Ok(usuarioDTO);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Apagar(int id)
+        {
+            var usuarioDTO = await _usuarioService.BuscarPorId(id);
+            if (usuarioDTO == null) return BadRequest("Usuário não encontrado");
+            await _usuarioService.Apagar(id);
+            return Ok(usuarioDTO);
+        }
+
+    }
+}
