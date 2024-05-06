9827e4fa16e6d2d9273fd40f15e8420a7f3f65b5
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2024-05-06 11:23:21 -0300

    feat(Migrations, UsuarioMap): The Migration was updated to implement the Usuario table in the database, and the UsuarioMap was modified to fix the Int type attributes in the database

--- api/Data/Map/UsuarioMap.cs
+++ api/Data/Map/UsuarioMap.cs
@@ -11,12 +11,12 @@ namespace api.Data.Map
             builder.HasKey(x => x.Id);
             builder.Property(x => x.CpfCnpj).IsRequired().HasMaxLength(50);
             builder.Property(x => x.NomeRazaoSocial).IsRequired().HasMaxLength(50);
-            builder.HasKey(x => x.RgLe);
+            builder.Property(x => x.RgLe);
             builder.Property(x => x.Logradouro).IsRequired().HasMaxLength(50);
-            builder.HasKey(x => x.Numero);
+            builder.Property(x => x.Numero);
             builder.Property(x => x.Cidade).IsRequired().HasMaxLength(50);
             builder.Property(x => x.Estado).IsRequired().HasMaxLength(50);
-            builder.HasKey(x => x.CEP);
+            builder.Property(x => x.CEP);
             builder.Property(x => x.Bairro).IsRequired().HasMaxLength(50);
             builder.Property(x => x.Rua).IsRequired().HasMaxLength(50);
             builder.Property(x => x.Email).IsRequired().HasMaxLength(50);
