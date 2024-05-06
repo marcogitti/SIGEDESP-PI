6bad415a03834021d4cf3a982d6ec95a85e9521e
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2024-05-06 09:51:27 -0300

    feat(UsuarioMap): The Map of the Usuario class was created, and the codes were implemented to organize the Model information in the database

---
+++ api/Data/Map/UsuarioMap.cs
@@ -0,0 +1,28 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace api.Data.Map
+{
+    public class UsuarioMap : IEntityTypeConfiguration<UsuarioModel>
+    {
+        public void Configure(EntityTypeBuilder<UsuarioModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.CpfCnpj).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.NomeRazaoSocial).IsRequired().HasMaxLength(50);
+            builder.HasKey(x => x.RgLe);
+            builder.Property(x => x.Logradouro).IsRequired().HasMaxLength(50);
+            builder.HasKey(x => x.Numero);
+            builder.Property(x => x.Cidade).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Estado).IsRequired().HasMaxLength(50);
+            builder.HasKey(x => x.CEP);
+            builder.Property(x => x.Bairro).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Rua).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Senha).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Situacao).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Matricula).IsRequired().HasMaxLength(50);
+        }
+    }
+}
