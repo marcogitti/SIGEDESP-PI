0af59280f80b3a23459a7a8588300fca70f86fa5
marcogitti <marcoalcindogitti@gmail.com>
2024-06-03 15:06:37 -0300

    fix(FornecedorDTO): Added more FornecedorDTO attributes

--- api/DTO/Entities/FornecedorDTO.cs
+++ api/DTO/Entities/FornecedorDTO.cs
@@ -17,6 +17,56 @@ namespace api.DTO.Entities
         [MaxLength(100)]
         public string Situacao { get; set; }
 
+        [Required(ErrorMessage = "O CNPJ é requerido!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Cnpj { get; set; }
+
+        [Required(ErrorMessage = "O Nome é requerido!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O Logradouro é requerido!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O Número é requerido!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public int Numero { get; set; }
+
+        [Required(ErrorMessage = "O Bairro é requerido!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "A Rua é requerida!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Rua { get; set; }
+
+        [Required(ErrorMessage = "O CEP é requerido!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public int Cep { get; set; }
+
+        [Required(ErrorMessage = "O Nome de Razão Social é requerido!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string nomeRazaoSocial { get; set; }
+
+        [Required(ErrorMessage = "O Telefone é requerido!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O E-mail é requerido!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
 
     }
 }
