62b5f8c6eb75902a2941944c759b5c93b59b288d
marcogitti <marcoalcindogitti@gmail.com>
2024-06-03 22:28:11 -0300

    fix(InstituicaoMap): Adding more attributes to InstituicaoMap

--- api/Data/Map/InstituicaoMap.cs
+++ api/Data/Map/InstituicaoMap.cs
@@ -10,6 +10,16 @@ namespace api.Data.Map
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Situacao).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Cnpj).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Nome).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Logradouro).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Numero).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Bairro).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Rua).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Cep).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.nomeRazaoSocial).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Telefone).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(50);
             builder.HasOne(x => x.tipoInstituicao).WithMany().HasForeignKey(x => x.IdTipoInstituicao);
             builder.HasOne(x => x.Secretaria).WithMany().HasForeignKey(x => x.IdSecretaria);
         }
